11c670fed6f9e1a2c84979b1f59a5a0e203d5d88
GayathriSS-19 <cp.gayathrisaisree@gmail.com>
2022-12-25 12:13:37 +0530

    Modified app.py and requuirements.txt

--- app.py
+++ app.py
@@ -18,35 +18,8 @@ def index():
         temp = float(form['temp'])
         p = []
         p += [area, item, year, rainfall, pesticides, temp]
-        # if gender == "M":
-        #     p+=[1]
-        # else:
-        #     p+=[0]
-        # if pclass == 2:
-        #     p+=[1,0]
-        # elif pclass == 3:
-        #     p+=[0,1]
-        # else:
-        #     p+=[0,0]
-        # if place.lower() == "queenstown":
-        #     p+=[1,0]
-        # elif place.lower() == "southampton":
-        #     p+=[0,1]
-        # else:
-        #     p+=[0,0]
-
-        # param_config = {
-        #     "Area": [0],
-        #     "Item": [1],
-        #     "Year": [0],
-        #     "average_rain_fall_mm_per_year": [1485.0],
-        #     'pesticides_tonnes': [121.00],
-        #     'avg_temp': [16.37]
-        # }
         result = py([p], final_model)
         return render_template('index.html', res=str(result))
-    # return res
-
     return render_template('index.html', res="Fill the details and Click Submit")
 
 
